3981bfc7843f602a814aa52fe3d06dad9cd8b9cf
Julia Faqir <julia.faqir@pwc.com>
2023-04-11 15:50:12 +0300

    Axios API call example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,104 +1,27 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { ThemeContextProvider } from "../components/context/ThemeContext";
-import { Box } from "../components/context/Box";
-import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
 
-
-const initialTodos = [
-  {
-    id: uuidv4(),
-    task: 'Learn React',
-    complete: true,
-  },
-  {
-    id: uuidv4(),
-    task: 'Learn Firebase',
-    complete: true,
-  },
-  {
-    id: uuidv4(),
-    task: 'Learn GraphQL',
-    complete: false,
-  },
-];
-
-export function Home() {
-  const [todos, setTodos] = useState(initialTodos);
-  const [task, setTask] = useState('');
-
-  const filterReducer = (state, action) => {
-    switch (action.type) {
-      case 'SHOW_ALL':
-        return 'ALL';
-      case 'SHOW_COMPLETE':
-        return 'COMPLETE';
-      case 'SHOW_INCOMPLETE':
-        return 'INCOMPLETE';
-      default:
-        throw new Error();
+const client = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com/posts" 
+});
+export const Home = () => {
+  const [post, setPost] = useState<any[]>([]);
+  useEffect(() => {
+    async function getPost() {
+      const response = await client.get("/1");
+      setPost(response.data);
     }
-  };
+    getPost();
+  }, []);
 
-  const [filter, dispatchFilter] = useReducer(filterReducer, 'ALL');
-
-  const handleShowAll = () => {
-    dispatchFilter({ type: 'SHOW_ALL' });
-  };
-
-  const handleShowComplete = () => {
-    dispatchFilter({ type: 'SHOW_COMPLETE' });
-  };
-
-  const handleShowIncomplete = () => {
-    dispatchFilter({ type: 'SHOW_INCOMPLETE' });
-  };
-
-  const filteredTodos = todos.filter(todo => {
-    if (filter === 'ALL') {
-      return true;
-    }
-
-    if (filter === 'COMPLETE' && todo.complete) {
-      return true;
-    }
-
-    if (filter === 'INCOMPLETE' && !todo.complete) {
-      return true;
-    }
-
-    return false;
-  });
-
-
-  const handleChangeCheckbox = id => {
-    setTodos(
-      todos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, complete: !todo.complete };
-        } else {
-          return todo;
-        }
-      })
-    );
-
-  };
-
-  const handleChangeInput = event => {
-    setTask(event.target.value);
-  };
-
-  const handleSubmit = event => {
-    if (task) {
-      setTodos(todos.concat({ id: uuidv4(), task, complete: false }));
-    }
-
-    setTask('');
-
-    event.preventDefault();
-  };
-
+  async function deletePost() {
+    await client.delete("/1");
+    alert("Post deleted!");
+    setPost(null);
+  }
+  if (!post) return "No post!"
   return (
     <>
       <Header />
@@ -108,55 +31,48 @@ export function Home() {
         height="919px"
         />
       <br />
-      <br />
-      <ThemeContextProvider>
-        <Box />
-      </ThemeContextProvider>
-      <br />
-      <br />
 
+      {/* <div>
+        <h3> Create post </h3>
+        <h4>{post.title}</h4>
+        <p>{post.body}</p>
+        <button onClick={createPost}>Create Post</button>
+      </div> */}
       <div>
-        <div>
-          <button type="button" onClick={handleShowAll}>
-            Show All
-          </button>
-          <button type="button" onClick={handleShowComplete}>
-            Show Complete
-          </button>
-          <button type="button" onClick={handleShowIncomplete}>
-            Show Incomplete
-          </button>
-        </div>
-
-      <ul>
-        {filteredTodos.map(todo => (
-          <li key={todo.id}>
-            <label>
-            <input
-                type="checkbox"
-                checked={todo.complete}
-                onChange={() => handleChangeCheckbox(todo.id)}
-              />  
-              {todo.task}
-            </label>
+      <h3> Get posts </h3>
+        <ul>
+          <li>
+            <h4>{post.title}</h4>
+            <p>{post.body}</p>
           </li>
-        ))}
-      </ul>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={task}
-          onChange={handleChangeInput}
-        />
-        <button type="submit">Add Todo</button>
-      </form>
+          <li>
+            <h4>{post.title}</h4>
+            <p>{post.body}</p>
+          </li>
+          <li>
+            <h4>{post.title}</h4>
+            <p>{post.body}</p>
+          </li>
+        </ul>
+      </div>
+      <br />
+      {/* <div>
+        <h3> Update post </h3>
+        <h4>{post.title}</h4>
+        <p>{post.body}</p>
+        <button onClick={updatePost}>Update Post</button>
+      </div> */}
+      <div>
+        <h3> Delete post </h3>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+        <button onClick={deletePost}>Delete Post</button>
       </div>
 
-
-      <br />
       <br />
       <Footer />
     </>
   );
 }
+
+
