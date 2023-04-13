d670d500e2ce0dae6ada9697c1a59ab23a60d30d
Julia Faqir <julia.faqir@pwc.com>
2023-04-13 16:37:54 +0300

    Homepage title change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Styling & Form Validation Task</Navbar.Brand>
+        <Navbar.Brand href="/">React Tables Task</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
